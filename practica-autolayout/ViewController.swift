4728c3e960237853acdc3705cbf8077d766c8b4e
Emiliano Mawe Macbook <emiliano@mawetecnologias.com>
2020-10-27 13:11:59 -0700

    Initial Commit

---
+++ practica-autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practica-autolayout
+//
+//  Created by Emiliano Lopez on 10/27/20.
+//  Copyright © 2020 Emiliano Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
